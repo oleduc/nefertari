efac2ac72a3d0c511f0f2f95c876c972d26469c1
nikitagromov <nikita.gromov@ardas.dp.ua>
2016-11-18 17:23:20 +0200

    beautify code

--- nefertari/elasticsearch.py
+++ nefertari/elasticsearch.py
@@ -775,6 +775,7 @@ class ES(object):
                 _params['body'] = {'query': {'bool': query}}
             else:
                 _params['body'] = {'query': {'match_all': {}}}
+
         if 'body' in params:
             raise JHTTPUnprocessableEntity('Illegal parameter "body"')
 
