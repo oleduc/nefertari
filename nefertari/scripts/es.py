acd3eaa619c512f831e2364d70aa64a71b66f6d9
nikitagromov <nikita.gromov@ardas.ua>
2017-05-10 17:00:23 +0300

    Split indexation into mupltiple processes

--- nefertari/scripts/es.py
+++ nefertari/scripts/es.py
@@ -1,9 +1,14 @@
 from argparse import ArgumentParser
 import sys
 import logging
+from multiprocessing import Pool, Process
+import math
+from functools import reduce
+from datetime import datetime
 
 from pyramid.paster import bootstrap
 from pyramid.config import Configurator
+from sqlalchemy import text
 from six.moves import urllib
 
 from nefertari.utils import dictset, split_strip, to_dicts, to_indexable_dicts
@@ -12,127 +17,263 @@ from nefertari import engine
 
 
 def main(argv=sys.argv, quiet=False):
+
+    parser = ArgumentParser(description=__doc__)
+
+    parser.add_argument(
+            '-c', '--config', help='config.ini (required)',
+            required=True)
+    parser.add_argument(
+            '--quiet', help='Quiet mode', action='store_true',
+            default=False)
+    parser.add_argument(
+            '--params', help='Url-encoded params for each model')
+    parser.add_argument('--index', help='Index name', default=None)
+    parser.add_argument(
+            '--chunk',
+            help=('Index chunk size. If chunk size not provided '
+                  '`elasticsearch.chunk_size` setting is used'),
+            type=int)
+
+    parser.add_argument('--processes',
+                        required=False,
+                        help='Split es action by multiple process',
+                        action='store',
+                        default=1,
+                        type=int)
+
+    parser.add_argument('--debug',
+                        required=False,
+                        help='Enable debug mode to check count for processed and existed item',
+                        action='store_true',
+                        default=False)
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+            '--models',
+            help=('Comma-separated list of model names to index'))
+    group.add_argument(
+            '--recreate',
+            help='Recreate index and reindex all documents',
+            action='store_true',
+            default=False)
+
+    options = parser.parse_args()
+
+    if not options.config:
+        return parser.print_help()
+
+    app_registry = setup_app(options=options, put_mappings=True)
+
+    processes = options.processes
+
+    if options.recreate:
+        recreate_index(app_registry, log)
+        models = engine.get_document_classes()
+        model_names = [
+                name for name, model in models.items()
+                if getattr(model, '_index_enabled', False)]
+    else:
+        model_names = split_strip(options.models)
+
+    tasks_pool = ESTasksPool(model_names, processes, options)
+
+    pool = Pool(processes)
+
+    processors = map(lambda i: ESProcessor(options=options, pool=tasks_pool, process_id=i,
+                                           model_names=model_names, processes_count=processes),
+                     range(0, processes))
+
+
+    if options.debug:
+        _check_results(pool.map(ESProcessor.apply, processors))
+    else:
+        pool.map(ESProcessor.apply, processors)
+
+
+def _check_results(result):
+    initial_dict = {}
+    flat_list = []
+    log = get_logger()
+    log.setLevel(logging.INFO)
+
+    for item in result:
+        flat_list.extend(item)
+
+    for item in flat_list:
+        for key in item:
+            if key not in initial_dict:
+                initial_dict[key] = []
+            initial_dict[key].extend(item[key])
+
+    for model_name in initial_dict:
+        items_count = engine.get_document_cls(model_name).get_collection().count()
+        log.info('Total items count {} for model {}'.format(items_count, model_name))
+        log.info('Total indexed tems count {} for model {}'.format(len(set(initial_dict[model_name])), model_name))
+
+        assert len(initial_dict[model_name]) == len(set(initial_dict[model_name]))
+        assert len(initial_dict[model_name]) == items_count
+
+
+def get_logger():
     log = logging.getLogger()
     log.setLevel(logging.WARNING)
     ch = logging.StreamHandler()
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
     ch.setFormatter(formatter)
     log.addHandler(ch)
-
-    command = ESCommand(argv, log)
-    return command.run()
+    return log
 
 
-class ESCommand(object):
+def setup_app(options, put_mappings):
+    # Prevent ES.setup_mappings running on bootstrap;
+    # Restore ES._mappings_setup after bootstrap is over
+    log = get_logger()
+    mappings_setup = getattr(ES, '_mappings_setup', False)
+    try:
+        ES._mappings_setup = True
+        env = bootstrap(options.config)
+    finally:
+        ES._mappings_setup = mappings_setup
+    registry = env['registry']
+    # Include 'nefertari.engine' to setup specific engine
+    config = Configurator(settings=registry.settings)
+    config.include('nefertari.engine')
+    ES.setup(dictset(registry.settings))
 
-    bootstrap = (bootstrap,)
-    stdout = sys.stdout
-    usage = '%prog config_uri <models'
+    if put_mappings:
+        ES.setup_mappings()
+    return registry
 
-    def __init__(self, argv, log):
-        parser = ArgumentParser(description=__doc__)
+def recreate_index(registry):
+    log = get_logger()
+    settings = dictset(registry.settings)
+    log.info('Deleting index')
+    ES.delete_index()
+    log.info('Creating index')
+    create_index_with_settings(settings)
+    self.log.info('Creating mappings')
+    ES.setup_mappings()
 
-        parser.add_argument(
-            '-c', '--config', help='config.ini (required)',
-            required=True)
-        parser.add_argument(
-            '--quiet', help='Quiet mode', action='store_true',
-            default=False)
-        parser.add_argument(
-            '--params', help='Url-encoded params for each model')
-        parser.add_argument('--index', help='Index name', default=None)
-        parser.add_argument(
-            '--chunk',
-            help=('Index chunk size. If chunk size not provided '
-                  '`elasticsearch.chunk_size` setting is used'),
-            type=int)
 
-        group = parser.add_mutually_exclusive_group(required=True)
-        group.add_argument(
-            '--models',
-            help=('Comma-separated list of model names to index'))
-        group.add_argument(
-            '--recreate',
-            help='Recreate index and reindex all documents',
-            action='store_true',
-            default=False)
+class ESTasksPool(object):
 
-        self.options = parser.parse_args()
-        if not self.options.config:
-            return parser.print_help()
-
-        # Prevent ES.setup_mappings running on bootstrap;
-        # Restore ES._mappings_setup after bootstrap is over
-        mappings_setup = getattr(ES, '_mappings_setup', False)
-        try:
-            ES._mappings_setup = True
-            env = self.bootstrap[0](self.options.config)
-        finally:
-            ES._mappings_setup = mappings_setup
-
-        registry = env['registry']
-        # Include 'nefertari.engine' to setup specific engine
-        config = Configurator(settings=registry.settings)
-        config.include('nefertari.engine')
-
-        self.log = log
-
-        if not self.options.quiet:
-            self.log.setLevel(logging.INFO)
-
-        self.settings = dictset(registry.settings)
-
-    def index_models(self, model_names):
-        self.log.info('Indexing models documents')
-        params = self.options.params or ''
-        params = dict([[k, v[0]] for k, v in urllib.parse.parse_qs(params).items()])
+    def __init__(self, model_names, processes, options):
+        self._pool = {process_id: [] for process_id in range(0, processes)}
+        self.options = options
 
         for model_name in model_names:
-            self.log.info('Processing model `{}`'.format(model_name))
+            for process_id in range(0, processes):
+                self._pool[process_id].append(ESTask(model_name, options))
+
+    def get_task(self, process_id):
+        if not self.is_empty(process_id):
+            return self._pool[process_id].pop()
+        return None
+
+    def is_empty(self, process_id):
+        return len(self._pool[process_id]) == 0
+
+
+class ESProcessor(object):
+
+    def __init__(self, options, pool, process_id, model_names, processes_count):
+        self.options = options
+        self.pool = pool
+        self.id = process_id
+        self.processes_count = processes_count
+        self.model_names = model_names
+
+    @staticmethod
+    def split_collection(limit, n, collection):
+        list_size = math.ceil(limit / n) - 1
+        iteration = 0
+
+        if not list_size:
+            list_size = 1
+
+        for i in range(0, limit, list_size):
+            iteration += 1
+
+            if iteration == n:
+                #yield i, limit - i
+                yield collection[i:]
+                break
+
+            yield collection[i: i + list_size]
+
+    def get_chunks(self):
+        model_chunks = {}
+        from nefertari import engine
+        from pyramid_sqlalchemy import Session
+
+        for model_name in self.model_names:
             model = engine.get_document_cls(model_name)
+            limit = model.get_collection().count()
+            table_name = model.__tablename__
+            statement = text('SELECT {} FROM public.{}'.format(model.pk_field(), table_name))
+            query = Session().query(model).from_statement(statement)
+            items = list(sorted(query.values(model.pk_field())))
+            chunks = list(self.split_collection(limit, self.processes_count, items))
 
-            local_params = dict()
-            local_params.update(params)
+            if len(chunks) - 1 < self.id:
+                model_chunks[model_name] = tuple()
+                continue
 
-            if '_limit' not in local_params:
-                limit = model.get_collection().count()
-                local_params['_limit'] = limit
+            model_chunks[model_name] = chunks[self.id]
+        return model_chunks
 
-            chunk_size = int(self.options.chunk or local_params['_limit'])
+    def __call__(self):
+        log = get_logger()
 
-            es = ES(source=model_name, index_name=self.options.index,
-                    chunk_size=chunk_size)
+        setup_app(self.options, put_mappings=False)
 
-            query_set = model.get_collection(**local_params)
-            documents = to_indexable_dicts(query_set)
-            self.log.info('Indexing missing `{}` documents'.format(
-                model_name))
-            es.index_missing_documents(documents)
-            es_actions = ESActionRegistry()
+        chunks = self.get_chunks()
+        results = []
 
-            for actions in es_actions.registry.values():
-                es_actions.force_indexation(actions=actions)
+        while not self.pool.is_empty(self.id):
+            task = self.pool.get_task(self.id)
+            ids = chunks[task.model_name]
+            results.append(task(ids))
 
-            es_actions.registry.clear()
+        log.info('indexing finished for process {} at {}'.format(self.id, str(datetime.now())))
+        return results
 
-    def recreate_index(self):
-        self.log.info('Deleting index')
-        ES.delete_index()
-        self.log.info('Creating index')
-        create_index_with_settings(self.settings)
-        self.log.info('Creating mappings')
-        ES.setup_mappings()
+    @staticmethod
+    def apply(processor):
+        return processor()
 
-    def run(self):
-        ES.setup(self.settings)
-        if self.options.recreate:
-            self.recreate_index()
-            models = engine.get_document_classes()
-            model_names = [
-                name for name, model in models.items()
-                if getattr(model, '_index_enabled', False)]
-        else:
-            ES.setup_mappings()
-            model_names = split_strip(self.options.models)
 
-        self.index_models(model_names)
+class ESTask(object):
+
+    def __init__(self, model_name, options):
+        self.model_name = model_name
+        self.options = options
+
+
+    def index_model(self, ids):
+        from nefertari import engine
+        from pyramid_sqlalchemy import Session
+
+        log = get_logger()
+        model = engine.get_document_cls(self.model_name)
+
+        chunk_size = int(self.options.chunk or len(ids))
+
+        es = ES(source=self.model_name, index_name=self.options.index, chunk_size=chunk_size)
+
+        query_set = Session().query(model).filter(getattr(model, model.pk_field()).in_(ids)).all()
+        ids = [getattr(item, item.pk_field()) for item in query_set]
+        documents = to_indexable_dicts(query_set)
+
+        es.index_missing_documents(documents)
+        es_actions = ESActionRegistry()
+
+        for actions in es_actions.registry.values():
+            es_actions.force_indexation(actions=actions)
+
+        es_actions.registry.clear()
+        return ids
+
+    def __call__(self, ids):
+        return {self.model_name: self.index_model(ids)}
